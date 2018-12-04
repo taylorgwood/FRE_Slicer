5565b3954f3e7ce3eca4b14a7f84f165e76d64f3
Taylor Greenwood <taylorgwood@gmail.com>
2018-12-04 15:57:56 -0700

    Added check for file name

--- shape.h
+++ shape.h
@@ -20,6 +20,9 @@ public:
     std::vector<Point>  get_points();
     std::vector<double> get_layer_locations();
     void generate_gcode(std::string fileName);
+    std::string check_file_name(std::string fileName, int numberOfChecks);
+    bool does_file_exist(const std::string& fileName);
+
 
 protected:
 
