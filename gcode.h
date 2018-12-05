80eef7ad3ceebe1e62c4b94a4c6950fd65cb3574
Taylor Greenwood <taylorgwood@gmail.com>
2018-12-05 15:37:09 -0700

    Extruder A and B volumes working. Length between points not working

--- gcode.h
+++ gcode.h
@@ -8,23 +8,34 @@ class Gcode
 {
 public:
     Gcode();
-    void generate_file(Shape &shape);
-    void write_gcode(std::ofstream& fout, Shape &shape);
+    void   generate_file(Shape &shape);
+    void   write_gcode(std::ofstream& fout, Shape &shape);
     std::ofstream create_empty_file();
     std::string get_file_name() const;
-    void set_file_name(std::string const fileName);
-    void write_layer_gcode(std::ofstream& fout, Shape& shape, int layerNumber);
-    void write_points_in_layer(std::ofstream& fout, Layer *layer);
-    void write_initial_gcode(std::ofstream& fout, Shape& shape);
-
-
+    void   set_file_name(std::string const fileName);
+    void   write_layer_gcode(std::ofstream& fout, Shape& shape, int layerNumber);
+    void   write_points_in_layer(std::ofstream& fout, Layer *layer);
+    void   write_initial_gcode(std::ofstream& fout, Shape& shape);
+    void   write_points_in_path(std::ofstream& fout, Path* path);
+    double get_extrusion_distance(double diameter, double mPointCount);
+    double get_extruder_A_displacement() const;
+    double get_extruder_B_displacement() const;
+    void   increment_extruder_A_displacement(double extruderAStep);
+    void   increment_extruder_B_displacement(double extruderBStep);
 
 protected:
 
 private:
-    void make_file_name_unique();
-    bool does_file_exist(const std::string& fileName);
+    void   make_file_name_unique();
+    bool   does_file_exist(const std::string& fileName);
     std::string mFileName{"newFile.txt"};
+    double mSyringeDiameter{14.9};
+    double mSyringeCrossSectionalArea{174.366246256};
+    double const pi{3.14159265359};
+    double mPointCount{0};
+    double mExtruderADisplacement{0};
+    double mExtruderBDisplacement{0};
+
 
 };
 
