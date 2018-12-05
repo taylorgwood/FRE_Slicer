b9dc8ce5f80eb6144b0a68f2cdbdc14f834457d5
Taylor Greenwood <taylorgwood@gmail.com>
2018-12-05 16:17:22 -0700

    Cleaning up code, added length calculation

--- gcode.h
+++ gcode.h
@@ -8,20 +8,22 @@ class Gcode
 {
 public:
     Gcode();
-    void   generate_file(Shape &shape);
-    void   write_gcode(std::ofstream& fout, Shape &shape);
-    std::ofstream create_empty_file();
+    void   generate_file(Shape& shape);
+    void   write_gcode(std::ofstream &fout, Shape &shape);
+    void create_empty_file();
+    std::ofstream get_fout();
     std::string get_file_name() const;
     void   set_file_name(std::string const fileName);
-    void   write_layer_gcode(std::ofstream& fout, Shape& shape, int layerNumber);
-    void   write_points_in_layer(std::ofstream& fout, Layer *layer);
-    void   write_initial_gcode(std::ofstream& fout, Shape& shape);
-    void   write_points_in_path(std::ofstream& fout, Path* path);
-    double get_extrusion_distance(double diameter, double mPointCount);
+    void   write_layer_gcode(std::ofstream& fout, Layer* layer);
+    void   write_points_in_layer(std::ofstream& fout, Layer* layer);
+    void   write_initial_gcode(std::ofstream& fout);
+    void   write_points_in_path(std::ofstream &fout, Path* path);
+    double get_extrusion_distance(double diameter, int mPointCount);
     double get_extruder_A_displacement() const;
     double get_extruder_B_displacement() const;
     void   increment_extruder_A_displacement(double extruderAStep);
     void   increment_extruder_B_displacement(double extruderBStep);
+    double calculate_length();
 
 protected:
 
@@ -35,7 +37,6 @@ private:
     double mPointCount{0};
     double mExtruderADisplacement{0};
     double mExtruderBDisplacement{0};
-
 
 };
 
